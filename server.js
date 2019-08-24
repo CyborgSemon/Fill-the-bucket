b97ba196b2cf4d9572cdb01d674647e77553dd76
Regan James <jamre937@student.otago.ac.nz>
2019-08-24 14:28:50 +1200

    update server

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const data = require('./data');
 
 
 app.get('/death/year=:year', (req, res)=> {
 
-    const year = req.params.year;
+    const year = parseFloat(req.params.year);
     const age = 2019 - year;
     let total = 0;
 
     for (let i = 0; i < age; i++) {
-      total += data[year][1 + i];
+      total += data[`${year + i}`][`${i + 1}`];
     }
 
-    res.send(`${year}, ${age}, ${total}`);
+    let newData = {
+      total : total
+    }
+
+    res.send(newData);
 
     // or send percentage of people?
 
